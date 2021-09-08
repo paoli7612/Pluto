ff3dfb5b92c6b2e35d0dc6bb7b998cfd26e81f0f
paoli7612 <paoli7612@gmail.com>
2021-09-08 18:56:09 +0200

    tailwindcss installed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Hello world</h1>
+    </body>
+</html>
