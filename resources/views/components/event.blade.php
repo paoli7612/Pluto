c31ca231280268408ae3949c088fdc34483abadf
paoli7612 <tomaoli7612@gmail.com>
2021-09-13 14:51:45 +0200

    view event static

--- resources/views/components/event.blade.php
+++ resources/views/components/event.blade.php
@@ -3,16 +3,23 @@
 <div class="card mb-3">
     <div class="card-header">
         <div class="float-left">
-            {{ $event['type'] }}
+            {{ $event->type }}
         </div>
         <div class="float-right">
-            {{ $event['date'] }}
+            
+            {{ date('D m M', strtotime($event->date)) }}
         </div>
     </div>
     <div class="card-body">
-        <div>Location</div>
-        <div>Waiters</div>
-        <div>Pax(intolerances)</div>
-        <div>Child</div>
+        <div>
+            Location: <b>Castel Pietra</b>
+        </div>
+        <div>
+            Waiters: <b>Valerio, Tommaso, Lorenzo</b>
+        </div>
+        <div>
+            Pax(intolerances): <b>45</b>(3 vegetariani)
+        </div>
+        <div>Child: <b>0</b></div>
     </div>
 </div>
