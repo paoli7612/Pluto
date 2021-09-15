2559f4b2f6201b3d0db3c467fe1c65a826e4c33f
paoli7612 <paoli7612@gmail.com>
2021-09-16 01:44:36 +0200

    event model factory migration controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome', [
-        'user' => auth()->user()
+        
     ]);
 });
 
@@ -26,3 +27,5 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/user/{user}', [UserController::class, 'show'])->name('user');
+
+Route::get('/events', [EventController::class, 'index'])->name('events');
